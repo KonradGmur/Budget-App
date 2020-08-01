f552e5aa6204803eb41d37db17ee3ed31ad0e57a
Konrad Gmur <konrad.gmur@gmail.com>
2020-08-01 13:09:50 +0200

    Application integration with the POEditor API

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,11 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
+import CustomBackend from './CustomBackend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const token = '4cfeaa05214ba122557e970429a11b88';
+const id = '318221';
+
 i18n
-    .use(Backend)
+    .use(CustomBackend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
@@ -15,9 +18,48 @@ i18n
         debug: true,
         saveMissing: true,
 
+        backend: {
+            loadPath: 'https://cors-anywhere.herokuapp.com/https://api.poeditor.com/v2/terms/list',
+            addPath: 'https://cors-anywhere.herokuapp.com/https://api.poeditor.com/v2/terms/add',
+            crossDomain: true,
+            parse: data => {
+                const parsedData = JSON.parse(data);
+                const terms = parsedData.result.terms.reduce((acc, item) => {
+                    acc[item.term] = item.translation.content || item.term;
+
+                    return acc;
+                }, {});
+
+                return terms;
+            },
+            parsePayload: (namespace, key) => {
+                if (key === '_t') return;
+
+                const data = [{
+                    term: key,
+                }];
+                const payload = {
+                    api_token: token,
+                    data: JSON.stringify(data),
+                    id,
+                };
+
+                return payload;
+            },
+            parseLoadPayload: ({ lng }) => {
+                const payload = {
+                    api_token: token,
+                    language: lng,
+                    id,
+                };
+
+                return payload;
+            },
+        },
+
         interpolation: {
-            escapeValue: false,
-        }
+            escapeValue: false, // not needed for react as it escapes by default
+        },
     });
 
 
