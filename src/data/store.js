f80b9b4ef061fe363dfb86f4db85c7fbdabf034a
Konrad Gmur <konrad.gmur@gmail.com>
2020-08-03 20:08:21 +0200

    Defining a Store for the Budget section

--- src/data/store.js
+++ src/data/store.js
@@ -8,11 +8,13 @@ export default function configureStore(preloadedState) {
         // thunkMiddleware
     ]
     const middlewareEnhancer = applyMiddleware(...middlewares)
-
     const enhancers = [middlewareEnhancer]
     const composedEnhancers = composeWithDevTools(...enhancers)
-
     const store = createStore(rootReducer, preloadedState, composedEnhancers)
+
+    if (process.env.NODE_ENV !== 'production' && module.hot) {
+        module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
+    }
 
     return store
 }
