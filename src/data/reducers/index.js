b1e6279662e22a826f19990143cd57b8c16f909e
Konrad Gmur <konrad.gmur@gmail.com>
2020-08-04 20:29:05 +0200

    Add redux-thund in project

--- src/data/reducers/index.js
+++ src/data/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import budget from './budget.reducer';
-import common from './common.reducer',
+import common from './common.reducer';
 
 const rootReducer = combineReducers({
     budget,
